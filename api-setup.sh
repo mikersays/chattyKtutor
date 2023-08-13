16fdc6a929ae7156a72f4ac54219f49e31bd457d
mikersays <129307237+mikersays@users.noreply.github.com>
2023-08-13 16:32:59 -0700

    Create api-setup.sh

---
+++ api-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Ask the user for their API key
+echo "Please enter your OpenAI API key:"
+read API_KEY
+
+# Use sed to replace the placeholder with the actual API key in chattytutor.py
+sed -i "s/<API key>/$API_KEY/g" chattytutor.py
+
+echo "API key set successfully!"
